79373a76c794f4df5deebac9229a276fe99f8fad
Matt March <matthewmarch29@gmail.com>
2023-06-10 16:26:09 +0100

    Move posts from gatsby site and add Markdown -> HTML via remark

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,17 +1,40 @@
 import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import { remark } from "remark";
+import html from "remark-html";
 
 type PostParams = {
   id: string;
 };
 
-export default ({ params }: { params: PostParams }) => <div>{params.id}</div>;
+export default async ({ params }: { params: PostParams }) => {
+  const fullPath = path.join(postsDirectory, `${params.id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const { data, content } = matter(fileContents);
+
+  const htmlContent = await remark().use(html).process(content);
+  const formattedDate = new Date(data.date).toLocaleDateString("en-GB");
+
+  return (
+    <div>
+      <h1 className="text-2xl">{data.title}</h1>
+      <div>Published {formattedDate}</div>
+      <article
+        className="prose lg:prose-xl dark:prose-invert"
+        dangerouslySetInnerHTML={{ __html: htmlContent.toString() }}
+      />
+    </div>
+  );
+};
 
 const postsDirectory = "posts";
 
 export const generateStaticParams = (): PostParams[] => {
   const fileNames = fs.readdirSync(postsDirectory);
   return fileNames.map((fileName) => ({
-    id: fileName,
+    id: fileName.replace(/\.md$/, ""),
   }));
 };
 
