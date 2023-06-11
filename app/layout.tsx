b19d4fd435dee167e3ebae7acddbb3e563bfa7fe
Matt March <matthewmarch29@gmail.com>
2023-06-11 19:44:56 +0100

    Add hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,29 @@ export const metadata = {
 export default ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Hero />
+        <div>{children}</div>
+      </body>
     </html>
   );
 };
+
+const Hero = () => (
+  <header className="h-80 relative">
+    <div className="-z-10">
+      <Image
+        priority
+        src="/bridge.jpg"
+        fill
+        alt="Photo of the Clifton Suspension Bridge"
+        className="object-cover object-center-[25%]"
+      />
+    </div>
+    <div className="relative flex flex-row justify-between h-full items-center p-24 text-3xl font-bold">
+      <a href="/" className="text-white">
+        Matt&apos;s Website
+      </a>
+    </div>
+  </header>
+);
