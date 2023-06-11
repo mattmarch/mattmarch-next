7f30a067b52dd6a23e99e38cd8dabb137892b629
Matt March <matthewmarch29@gmail.com>
2023-06-11 21:04:41 +0100

    Add all the details to index page

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,47 @@ export default async () => {
 
   return (
     <main className="min-h-screen md:p-24 p-8 prose lg:prose-xl dark:prose-invert ">
-      <h1>Hi, I&apos;m Matt March, welcome to my website</h1>
-
-      <h2>Recent posts</h2>
+      <h1>Hi, I&apos;m Matt March, welcome to my website.</h1>
+      <p>
+        I&apos;m a Bristol based software engineer currently working at{" "}
+        <a href="https://www.ghyston.com/">Ghyston</a>. You can find me on:
+      </p>
       <ul>
+        <li>
+          <a href="https://github.com/mattmarch">Github</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/matthew-march-a75b21121/">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="mailto:me@mattmarch.co.uk">Email</a>
+        </li>
+      </ul>
+
+      <h2>Projects</h2>
+      <p>Check out some of the fun side projects I&apos;ve been working on:</p>
+      <ul>
+        <li>
+          <a href="https://lease-or-no-lease.mattmarch.co.uk">
+            Lease or no lease
+          </a>
+          , does your company offer an EV leasing scheme and have you wondered
+          how the cost of ownership compares to a 2nd hand petrol car?
+          Here&apos;s an easy way to run the numbers!
+        </li>
+        <li>
+          <a href="https://mattmarch.itch.io/rgb-pigs">RGB Pigs</a>, a topdown
+          survival game where you must toggle your flashlight&apos;s colour to
+          tell friend from foe. Playable in browser and built with{" "}
+          <a href="https://godotengine.org/">Godot Engine.</a>
+        </li>
+      </ul>
+
+      <h2>Blog posts</h2>
+      <p>And here are some blog posts I&apos;ve put together:</p>
+      <ul className="space-y-10">
         {sortedPosts.map((post) => (
           <PostListItem key={post.id} post={post} />
         ))}
