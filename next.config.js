b15e5c21ccb58222d6b3158c7eccd5efc5b5cc83
Matt March <matthewmarch29@gmail.com>
2023-06-11 11:22:34 +0100

    Add redirects for old gatsby routes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: () => [
+    // Redirects paths used in old Gatsby blog
+    {
+      source: "/new-website",
+      destination: "/posts/gatsby-website",
+      permanent: true,
+    },
+    {
+      source: "/birthday-playlist",
+      destination: "/posts/birthday-playlist",
+      permanent: true,
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
